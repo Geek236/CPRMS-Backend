c92467ce8cdef133291e800b05ba70d2eef5a68f
Geek236 <iamd3cxty@gmail.com>
2026-01-20 09:25:23 +0200

    Update db.cjs

--- config/db.cjs
+++ config/db.cjs
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL_OFFLINE);
+    await mongoose.connect(process.env.MONGO_URL);
     console.log('MongoDB connected successfully');
   } catch (error) {
     console.error('MongoDB connection failed:', error.message);
@@ -11,3 +11,4 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
+
