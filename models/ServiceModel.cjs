905220e4bd991fa6b41046f236c04e8c63820374
Geek236 <iamd3cxty@gmail.com>
2026-01-20 08:06:32 +0200

    Delete models directory

--- models/ServiceModel.cjs
+++
@@ -1,9 +0,0 @@
-const mongoose= require("mongoose")
-const serviceModel= new mongoose.Schema({
-    ServiceName:{type:String, required:true},
-    ServicePrice:{type:Number,required:true},
-    ServiceDescription:{type:String, required:false}
-   
-})
-const ServiceModel= mongoose.model('Services',serviceModel)
-module.exports= ServiceModel;
